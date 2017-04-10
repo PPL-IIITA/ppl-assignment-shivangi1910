63d5cbe56f46980a15afac812d4fe4e752b62473
Shivangi Singhal <rit2015011@iiita.ac.in>
2017-04-10 08:09:32 +0530

    Question5

---
+++ Q5/src/q5/Couple.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package q5;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author shivangi
+ */
+public class Couple {
+    
+    Boy boy;
+    Girl girl;
+    private int happiness;
+    private int compatibility;
+    public static ArrayList<Gift> gifts = new ArrayList<>();
+    
+    Couple(Boy a,Girl b) {
+        this.boy = a;
+        this.girl = b;
+        this.happiness = 0;
+        this.compatibility = 0;
+    }
+
+    void sethappiness() {
+        this.girl.sethappiness(gifts);
+        this.happiness = this.girl.gethappiness() + this.boy.gethappiness();
+        //this.happiness = h;
+    }
+    
+    void setcompatibility(int v) {
+        this.compatibility = v;
+    }
+    Boy getBoy() {
+        return this.boy;
+    }
+    
+    Girl getGirl() {
+        return this.girl;
+    }
+    
+    int gethappiness() {
+        return this.happiness;
+    }
+    
+    int getcompatibility() {
+        return this.compatibility;
+    }   
+    
+    ArrayList<Gift> giveGift(ArrayList<Gift> Gifts) {
+        ArrayList<Gift> g = this.boy.giveGift(Gifts);
+        this.getGirl().sethappiness(g);
+        return g;
+    }
+}
---
+++ Q5/src/q5/Utility.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package q5;
+
+/**
+ *
+ * @author shivangi
+ */
+public class Utility extends Gift{
+    
+    int uclass;
+    int uvalue;
+    
+    Utility(String name,int price, int value, int uclass, int uvalue)
+    {
+        super(name,price,value);
+        this.uclass = uclass;
+        this.uvalue = uvalue;
+        this.setType(3);
+    }
+
+       
+}
